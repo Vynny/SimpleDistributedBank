ad38c3a8ae3f04625d8ebc785babc2e0e28993d0
SomeMatthew <someMathew@outlook.com>
2017-11-14 00:52:29 -0500

    Initial Message Format

---
+++ DBS_Project/src/dbs/branches/message/MessageBody.java
@@ -0,0 +1,7 @@
+package dbs.branches.message;
+
+import java.io.Serializable;
+
+public interface MessageBody extends Serializable {
+
+}
---
+++ DBS_Project/src/dbs/branches/message/MessageFault.java
@@ -0,0 +1,14 @@
+package dbs.branches.message;
+
+import java.io.Serializable;
+
+public class MessageFault implements Serializable {
+	
+	public String errorId;
+	public String errorMessage;
+	
+	public MessageFault() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
