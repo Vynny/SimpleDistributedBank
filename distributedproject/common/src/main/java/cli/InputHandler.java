685fff37e9547c8bec7b9462eddf6c32bc336a70
vynny <sylvain.czyzewski@gmail.com>
2017-11-27 17:08:50 -0500

    -Added CustomerClient -Added util package to frontend containing CORBAConnector to resolve front end in client

---
+++ distributedproject/common/src/main/java/cli/InputHandler.java
@@ -0,0 +1,58 @@
+package cli;
+
+import java.util.Scanner;
+
+public abstract class InputHandler {
+
+    private String userType;
+
+    public InputHandler(String userType) {
+        this.userType = userType;
+    }
+
+    public void gatherInput() {
+        System.out.println("Awaiting input. Type help for more information. Type exit to quit. \n");
+
+        Scanner sc = new Scanner(System.in);
+
+        String input;
+        while ((input = sc.nextLine()) != null) {
+            if (validateInput(input)) {
+                if (!processActionString(input))
+                    parseInput(input);
+            } else {
+                System.out.println("Invalid command entered");
+            }
+        }
+    }
+
+    protected abstract void parseInput(String input);
+
+    protected abstract Enum<?> validateAction(String[] input);
+
+    protected abstract void printHelp();
+
+    private boolean validateInput(String input) {
+        if (input == null || input.isEmpty())
+            return false;
+        return true;
+    }
+
+    private boolean processActionString(String input) {
+        if (input.toLowerCase().equals("help")) {
+            printHelp();
+            return true;
+        }
+
+        if (input.toLowerCase().equals("exit")) {
+            System.out.println("Thank you for using " + userType + " Client.");
+            System.exit(0);
+        }
+
+        return false;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+}
