483acfaa6beca8c512ae9d64d8053a9e8e0c2509
SomeMatthew <someMathew@outlook.com>
2017-12-01 22:39:26 -0500

    Concurrency Test Client

--- distributedproject/customerclient/src/main/java/test/CustomerClientTest.java
+++ distributedproject/customerclient/src/main/java/test/CustomerClientTest.java
@@ -3,43 +3,44 @@ package test;
 import cli.ClientInputHandler;
 
 public class CustomerClientTest implements Runnable {
-	private static String[] test1 = { "deposit QCC1000 50", "deposit QCC1000 50", "withdraw QCC1000 100",
-			"getBalance QCC1000" };
+    private static String[] test0 = { "deposit QCC1000 50" };
+    private static String[] test1 = { "deposit QCC1000 50", "deposit QCC1000 50", "withdraw QCC1000 100",
+            "getBalance QCC1000" };
 
-	private String[] inputs;
-	private ClientInputHandler client;
+    private String[] inputs;
+    private ClientInputHandler client;
 
-	public CustomerClientTest(String[] inputs) {
-		this.inputs = inputs;
-		this.client = new ClientInputHandler("Customer");
-	}
+    public CustomerClientTest(String[] inputs) {
+        this.inputs = inputs;
+        this.client = new ClientInputHandler("Customer");
+    }
 
-	public static void main(String args[]) {
-		startTestThreads(test1, 10);
-	}
+    public static void main(String[] args) {
+        startTestThreads(test1, 10);
+    }
 
-	private static void startTestThreads(String[] inputs, int size) {
-		Thread[] threads = new Thread[size];
+    private static void startTestThreads(String[] inputs, int size) {
+        Thread[] threads = new Thread[size];
 
-		for (int i = 0; i < threads.length; i++) {
-			CustomerClientTest client = new CustomerClientTest(inputs);
-			threads[i] = new Thread(client);
-			threads[i].start();
-		}
+        for (int i = 0; i < threads.length; i++) {
+            CustomerClientTest client = new CustomerClientTest(inputs);
+            threads[i] = new Thread(client);
+            threads[i].start();
+        }
 
-		for (Thread t : threads) {
-			try {
-				t.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	@Override
-	public void run() {
-		for (String s : inputs) {
-			client.parseInput(s);
-		}
-	}
+    @Override
+    public void run() {
+        for (String s : inputs) {
+            client.parseInput(s);
+        }
+    }
 }
